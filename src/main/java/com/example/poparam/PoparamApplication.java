0e0821b9b56cc81e4e2cb28282f1c246aeb05bd8
WoldemarK <kovtynov.vladimir@gmail.com>
2023-05-31 02:46:49 +0300

    Первичная настройка проекта

---
+++ src/main/java/com/example/poparam/PoparamApplication.java
@@ -0,0 +1,13 @@
+package com.example.poparam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoparamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoparamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/poparam/PoparamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poparam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoparamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
